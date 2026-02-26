c939f2d6896cc6347e17ee6f7ae71dc0980e3fc0
Jonikii <jonasmatej3@gmail.com>
2026-02-26 08:51:15 +0100

    3

--- flask/app.py
+++ flask/app.py
@@ -5,10 +5,13 @@ app = Flask(__name__)
 def home():
     return "Hello World!"
 
-
 @app.route("/2")
 def html_basic():
     return render_template("index2.html")
+
+@app.route("/3")
+def hrasek():
+    return render_template("index3.html")
 
 if __name__ == "__main__":
     app.run()
