5fa0f7bed6b3fd5c3f58dc7f4b10cff5b24bb483
Jonikii <jonasmatej3@gmail.com>
2026-02-26 08:35:42 +0100

    plesk

--- flask/app.py
+++ flask/app.py
@@ -1,9 +1,14 @@
-import flask
+from flask import Flask, render_template
 
-app = flask.Flask(__name__)
-@app.route("/")
-def hello():
+app = Flask(__name__)
+@app.route("/1")
+def home():
     return "Hello World!"
+
+
+@app.route("/2")
+def html_basic():
+    return render_template("index2.html")
 
 if __name__ == "__main__":
     app.run()
