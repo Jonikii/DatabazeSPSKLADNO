b2703143cf9d8895b55f1275047dc51cbb030ec0
Jonikii <jonasmatej3@gmail.com>
2026-03-19 09:31:53 +0100

    9

--- flask/app.py
+++ flask/app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, url_for, request
 import os
+import plotly.graph_objects as go
+import plotly.io as pio
 
 app = Flask(__name__)
 @app.route("/1")
@@ -51,6 +53,44 @@ def nahrani_souboru():
     return render_template('index7.html', content=content)
 
 
+@app.route('/8') 
+def graph():
+    fig = go. Figure()
+    fig.add_trace(go.Scatter (x=[1, 2, 3, 4], y=[10, 20, 25, 30], 
+                              mode='lines+markers', name='Data 1')) 
+    fig.add_trace(go.Scatter (x=[1, 2, 3, 4], y=[15, 18, 22, 27], 
+                              mode='lines+markers', name='Data 2'))
+    fig.update_layout(
+        title="Ukázkový interaktivní graf",
+        xaxis_title="X-osa",
+        yaxis_title="Y-osa",
+        template="plotly_white"
+)
+    graph_html = pio.to_html(fig, full_html=False) # HTML fragment return render_template("index8.html", graph_html=graph_html)
+    return render_template("index8.html", graph_html=graph_html)
+
+@app.route('/8/2') 
+def graph2():
+    fig = go. Figure()
+    fig.add_trace(go.Scatter (x=[1, 2, 3, 4], y=[10, 20, 25, 30], 
+                              mode='stack', name='Data 1')) 
+    fig.add_trace(go.Scatter (x=[1, 2, 3, 4], y=[15, 18, 22, 27], 
+                              mode='stack', name='Data 2'))
+    fig.update_layout(
+        title="Ukázkový interaktivní graf",
+        xaxis_title="X-osa",
+        yaxis_title="Y-osa",
+        template="plotly_white"
+)
+    graph_html = pio.to_html(fig, full_html=False) # HTML fragment return render_template("index8.html", graph_html=graph_html)
+    return render_template("index8.html", graph_html=graph_html)
+
+
+@app.route('/9/<int:id>/<string:name>', methods=['GET'])
+def parametry(id, name):
+    return render_template("index9.html", id=id, name=name)
+
+
+
 if __name__ == "__main__":
     app.run()
-
